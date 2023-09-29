2cbca91ce06011de95217ca40ed676fb7cbbaae1
TheDumbCoder <119265202+TheHexCoder0x10@users.noreply.github.com>
2023-09-29 17:20:41 -0500

    Update installer batch file

--- installer.bat
+++ installer.bat
@@ -1,5 +1,6 @@
 @echo on
 mkdir C:\Users\%USERNAME%\AppData\Local\AUSTENS_CALCULATOR
-powershell -Command Invoke-WebRequest https://www.github.com/TheHexCoder0x10/Calc -OutFile C:\Users\%USERNAME%\AppData\Local\AUSTENS_CALCULATOR\Files.zip
+powershell -Command Invoke-WebRequest https://www.github.com/TheHexCoder0x10/Calc/raw/main/Calc.zip -OutFile C:\Users\%USERNAME%\AppData\Local\AUSTENS_CALCULATOR\Files.zip
+powershell -Command Invoke-WebRequest https://www.github.com/TheHexCoder0x10/Calc/raw/main/Calculator.bat -OutFile C:\Users\%USERNAME%\AppData\Local\AUSTENS_CALCULATOR\Calc.bat
 powershell -Command Expand-Archive -Force C:\Users\%USERNAME%\AppData\Local\AUSTENS_CALCULATOR\Files.zip C:\Users\%USERNAME%\AppData\Local\AUSTENS_CALCULATOR\
 pause
